e3af69f930009685b5a749a0e680a2b1756a75e6
John Mulligan <jcm7211@rit.edu>
2026-04-09 14:50:43 -0400

    added some vars for live game stuff

--- client/src/components/chess/PlayerProfile/PlayerProfileProps.ts
+++ client/src/components/chess/PlayerProfile/PlayerProfileProps.ts
@@ -3,11 +3,13 @@ import PieceColour from "shared/constants/PieceColour";
 
 interface PlayerProfileProps {
     profile: PlayerProfile;
+    profileUrl?: string;
     playerColour?: PieceColour;
     currentFen?: string;
     showClock?: boolean;
     clockTimeMs?: number;
     clockActive?: boolean;
+    clockRealtime?: boolean;
 }
 
 export default PlayerProfileProps;
